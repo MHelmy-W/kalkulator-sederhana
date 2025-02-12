83bbb7d3f27d6683da399f01709ce98dfb312a6a
MHelmy-W <mhelmy4911@gmail.com>
2025-02-12 11:30:49 +0700

    first initial

---
+++ app/src/main/java/com/example/tentangsaya/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tentangsaya
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tentangsaya.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        handleUser()
+    }
+
+    private fun handleUser() {
+        binding.textView3.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Tentang Saya")
+            builder.setMessage("Tentang Saya : Saya adalah murid kelas 12")
+            builder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+            Toast.makeText(applicationContext, "Tentang Saya", Toast.LENGTH_LONG).show()
+        }
+        binding.calcuApps.setOnClickListener {
+            val intent = Intent(this, CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tentangsaya/MainActivity2.kt
@@ -0,0 +1,38 @@
+package com.example.tentangsaya
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
+
+class Login : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        val emailEditText = findViewById<EditText>(R.id.editTextEmail)
+        val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email dan password harus diisi", Toast.LENGTH_SHORT).show()
+            } else if (email == "mhelmy4911@gmail.com" && password == "4911") {
+                Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Email atau password salah", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
